0f0fc96b28d3d7f57ecd0f40e9bb329775060321
Arturo R <jaquer@frink>
2004-09-06 04:01:46 +0000

    Recurse through files to determine rip's flags.

--- update_db.php
+++ update_db.php
@@ -4,13 +4,16 @@
 /*
  * update_db.php - Script to update the Music Content Management DB
  *
- * $Header: /var/oldvar/cvsroot/jaquer/new-mcm/update_db.php,v 1.3 2004/09/06 03:47:49 jaquer Exp $
+ * $Header: /var/oldvar/cvsroot/jaquer/new-mcm/update_db.php,v 1.4 2004/09/06 04:01:46 jaquer Exp $
  *
  */
  
 require_once('mcm_defs.inc');
 
 $name_regexp = "/^(\[.*\]) (\[.*\]) (\[.*\])$/";
+$pun_regexp  = "/\.pun$/";
+$log_regexp  = "/\.log$/";
+$bad_regexp  = "/^--NOT COMPLIANT WITH STANDARD--\.txt$/";
 
 if (is_dir($music_dir)) {
 
@@ -31,8 +34,20 @@ if (is_dir($music_dir)) {
       $album   = $matches[2];
       $quality = $matches[3];
       
+      
+      $path_handle = opendir($path);
+      
+      /* Now recurse through files in each dir, and determine the rip's flags */
+      while ( $file = readdir($path_handle) ) {
+
+        $has_pun = (preg_match($pun_regexp, $file)) ? TRUE : $has_pun;
+        $has_log = (preg_match($log_regexp, $file)) ? TRUE : $has_log;
+        $has_bad = (preg_match($bad_regexp, $file)) ? TRUE : $has_bad;
+        
+      }
+      closedir($path_handle);
+      
     }
-    
     closedir($base_handle);
     
   }
