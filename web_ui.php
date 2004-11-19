db10571ac4dc27f9c23042a371c34c8d880942e8
Arturo R <jaquer@frink>
2004-11-19 08:08:19 +0000

    Print list of rips.

--- web_ui.php
+++ web_ui.php
@@ -10,7 +10,6 @@
 require_once('mcm_defs.inc');
 session_start();
 
-
 $user_name          = (isset($_POST['user_name'])) ? $_POST['user_name'] : "";
 $cleartext_password = (isset($_POST['password'])) ? $_POST['password'] : "";
 $encoded_password   = (isset($_SESSION['encoded_password'])) ? $_SESSION['encoded_password'] : md5($cleartext_password);
@@ -55,10 +54,12 @@ include('html_footer.inc');
 
 function print_rips_list($user_id, $start, $status) {
 
+  $limit = 30;
   $user = lookup_user($user_id);
 
   $start = ( $status == "Next" ) ? $start + $limit : $start;
   $start = ( $status == "Prev" ) ? $start - $limit : $start;
+  $_POST['start'] = $start;
 ?>
     <p style="font-size: 120%; font-weight: bold;">Welcome <?= $user['user_name']; ?>. Your last visit was on: <?= $user['user_last_visit']; ?></p>
 
@@ -73,8 +74,116 @@ function print_rips_list($user_id, $start, $status) {
   }
 ?>
       <!-- End previous page data -->
+      <table>
+        <tr>
+          <th>Artist</th>
+          <th>Album</th>
+          <th>Yes</th>
+          <th>No</th>
+          <th>Maybe</th>
+          <th>Quality</th>
+          <th>Flags</th>
+          <th>Added</th>
+          <th>Play</th>
+        </tr>
 <?php
 
+  $query = "
+
+  SELECT 
+    COUNT(*) AS num_rips
+  FROM
+    mdb_rip
+  LEFT JOIN
+    mdb_reviewed
+  ON
+    mdb_rip.rip_id = mdb_reviewed.rip_id
+  AND
+    mdb_reviewed.user_id = $user_id
+  WHERE 
+    mdb_reviewed.rip_id IS NULL
+  
+  ";
+
+  $row = get_row_q($query);
+  $num_rips = $row['num_rips'];
+
+  $query = "
+
+  SELECT
+    mdb_rip.rip_id,
+    mdb_rip.artist_id,
+    mdb_rip.album_id,
+    mdb_artist.artist_name,
+    mdb_album.album_name,
+    mdb_rip.rip_quality,
+    mdb_rip.rip_flags,
+    mdb_rip.rip_added
+  FROM
+    mdb_rip,
+    mdb_artist,
+    mdb_album
+  LEFT JOIN
+    mdb_reviewed
+  ON
+    mdb_rip.rip_id = mdb_reviewed.rip_id
+  AND
+    mdb_reviewed.user_id = $user_id
+  WHERE
+    mdb_rip.artist_id = mdb_artist.artist_id
+  AND
+    mdb_rip.album_id = mdb_album.album_id
+  AND
+    mdb_reviewed.rip_id IS NULL
+  ORDER BY
+    artist_name,
+    album_name,
+    rip_quality
+  LIMIT 
+    $start, $limit
+
+  ";
+
+
+  $result = do_query($query);
+  $row_number = 0;
+
+  while ( $row = get_row_r($result) ) {
+    $row_number++;
+?>
+        <tr<?= ($row_number % 2) ? ' class="highlight"' : ""; ?>>
+          <!-- ID: <?= $row['rip_id']; ?> -->
+          <td><?= htmlentities($row['artist_name']); ?></td>
+          <td><?= htmlentities($row['album_name']); ?></td>
+          <td><input type="radio" name="id<?php echo $row['rip_id']; ?>" value="accepted"<?= print_checkbox($row['rip_id'], "accepted"); ?>></td>
+          <td><input type="radio" name="id<?= $row['rip_id']; ?>" value="rejected"<?= print_checkbox($row['rip_id'], "rejected"); ?>></td>
+          <td><input type="radio" name="id<?= $row['rip_id']; ?>" value="maybe"<?= print_checkbox($row['rip_id'], "maybe"); ?>></td>
+          <td<?= ($row['rip_quality'] == "APX") ? ' class="flag"' : ""; ?>><?= $row['rip_quality']; ?></td>
+          <td><?= ( $row['rip_flags'] != "" ) ? $row['rip_flags'] : "&nbsp;"; ?></td>
+          <td><?= $row['rip_added']; ?></td>
+          <td><a href="<?= "/zina/index.php?p=[" . $row['artist_name'] . "] [" . $row['album_name'] . "] [" . $row['rip_quality'] . "]&l=8&m=0"; ?>">Go</a></td>
+        </tr>
+<?php
+  }
+?>
+      </table>
+      <p><?= ($start - $limit >= 0) ? '<input type="submit" name="submit" value="Prev">' : ''; ?> <?= ($start + $limit <= $num_rips) ? '<input type="submit" name="submit" value="Next">' : '' ?></p>
+      <p><input type="submit" name="submit" value="Finish"></p>
+<?php
+}
+
+function print_checkbox($id, $value) {
+  
+  $id = "id" . $id;
+  
+  if ( (isset($_POST[$id])) && ($_POST[$id] == $value) ) {
+    return " checked";
+  }
+
+  if ( ( ! isset($_POST[$id]) && ($value == "maybe" ) )) {
+    return " checked";
+  }
+  
 }
 
 
