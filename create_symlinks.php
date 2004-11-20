51c4938370d997bfb868a0dffe557f0ad47846ec
Arturo R <jaquer@frink>
2004-11-20 07:50:05 +0000

    Initial argument parsing, error catching.

--- create_symlinks.php
+++ create_symlinks.php
@@ -8,4 +8,30 @@
  *
  */
 
+require_once('mcm_defs.inc');
+
+if ( $argc < 3 || $argc > 3 ) die("Usage: " . $argv[0] . " username dest_dir\n");
+
+$user_name = $argv[1];
+$dest_dir  = realpath($argv[2]);
+
+if ( ! is_dir($dest_dir) ) die("Directory '$dest_dir' does not exist.\n");
+if ( ! touch($dest_dir . "/test.tmp") )
+  die("Cannot write to '$dest_dir'\n");
+else
+  unlink($dest_dir . "/test.tmp");
+
+if ( ! $user_id = get_user_id($user_name) )
+  die("User: '$user_name' does not exist.\n");
+
+
+
+function get_user_id($user_name) {
+  
+  $query = "SELECT user_id FROM mdb_user WHERE user_name = '$user_name'";
+  $row = get_row_q($query);
+  return $row['user_id'];
+  
+}
+
 ?>
