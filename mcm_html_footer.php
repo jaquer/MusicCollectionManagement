0c4e7f1b0e120b80df9c04370eba83f64a716fb5
Arturo R <jaquer@frink>
2009-06-19 04:00:07 +0000

    Cleaned up footer.

--- mcm_html_footer.php
+++ mcm_html_footer.php
@@ -3,9 +3,10 @@
   /* mcm_html_footer.php - prints an html footer (yes, really) */
   
 function mcm_html_footer($params) {
+
+  if ($params) echo '    <p id="last-visit">' . $params['user_name'] . ' &bull; last visit: ' . $params['last_visit'] . '</p>';
 ?>
 
-    <?php if ($params) echo '<p id="last-visit">' . $params['user_name'] . ' &bull; last visit: ' . $params['last_visit'] . '</p>'; ?>
   </body>
 </html>
 <?php
